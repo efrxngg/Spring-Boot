cb25e4d2742f708e84c626c9e08c08f786d910c1
Pasante5 desa <p5@rgtdom.local>
2022-08-10 14:57:30 -0500

    entidades js

--- src/main/resources/static/js/insertOrden.js
+++ src/main/resources/static/js/insertOrden.js
@@ -1,3 +1,24 @@
+class Producto{
+    constructor(cantidad, ingrediente){
+        this.cantidad = cantidad;
+        this.ingrediente = ingrediente;
+    }
+}
+// listaProductos = [new Producto(1, 1)]
+
+
+class DetalleOrden{
+    constructor(cantidad, base_pizza, comentario, otros = 0, ingredientes){
+        this.cantidad = cantidad;
+        this.base_pizza = base_pizza;
+        this.comentario = comentario;
+        this.otros = otros;
+        this.ingredientes = ingredientes;
+    }
+}
+// var listaDetalles = [new DetalleOrden(1, 1, "Sin bordes", 0, listaProductos)]
+
+
 class Orden{
     constructor(nombre, contacto, detallesDeLaOrden){
         this.nombre = nombre;
@@ -5,8 +26,11 @@ class Orden{
         this.detallesDeLaOrden = detallesDeLaOrden;
     }
 }
+// var test = new Orden("Efren", "0997188086", listaDetalles);
+// console.log(JSON.stringify(test));
 
 
+listaProductos = [];
+listaDetalleOrden = [];
+listaOrden = [];
 
-var test = new Orden("Efren", "0997188086", []);
-console.log(JSON.stringify(test));
